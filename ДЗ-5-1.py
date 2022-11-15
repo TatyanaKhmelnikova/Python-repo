717c3a6a8b9d4b9592db4c89b889a3a1e848419e
Tatyana <propan_212015@mail.ru>
2022-11-15 07:55:24 +0300

    ДЗ-5 Python

---
+++ ДЗ-5-1.py
@@ -0,0 +1,11 @@
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+words = input('Введите текст через пробел: ').split(' ')
+fragment = 'абв'
+new_words = []
+for i in words:
+    if fragment not in i:
+        new_words.append(i)
+print(new_words)
+
+
